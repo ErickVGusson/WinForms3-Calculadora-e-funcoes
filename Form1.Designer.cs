3e8d35c8bcdb5fbe686c8805e30e63b096f2fb45
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2023-11-07 08:47:29 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbNumero1 = new System.Windows.Forms.TextBox();
+            this.txbNumero2 = new System.Windows.Forms.TextBox();
+            this.btnMais = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnVezes = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbNumero1
+            // 
+            this.txbNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNumero1.Location = new System.Drawing.Point(31, 98);
+            this.txbNumero1.Name = "txbNumero1";
+            this.txbNumero1.Size = new System.Drawing.Size(68, 23);
+            this.txbNumero1.TabIndex = 4;
+            // 
+            // txbNumero2
+            // 
+            this.txbNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNumero2.Location = new System.Drawing.Point(105, 98);
+            this.txbNumero2.Name = "txbNumero2";
+            this.txbNumero2.Size = new System.Drawing.Size(68, 23);
+            this.txbNumero2.TabIndex = 5;
+            // 
+            // btnMais
+            // 
+            this.btnMais.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMais.Location = new System.Drawing.Point(31, 124);
+            this.btnMais.Name = "btnMais";
+            this.btnMais.Size = new System.Drawing.Size(68, 61);
+            this.btnMais.TabIndex = 6;
+            this.btnMais.Text = "+";
+            this.btnMais.UseVisualStyleBackColor = true;
+            this.btnMais.Click += new System.EventHandler(this.btnMais_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenos.Location = new System.Drawing.Point(105, 124);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(68, 61);
+            this.btnMenos.TabIndex = 7;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(105, 191);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(68, 61);
+            this.btnDividir.TabIndex = 8;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnVezes
+            // 
+            this.btnVezes.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVezes.Location = new System.Drawing.Point(31, 191);
+            this.btnVezes.Name = "btnVezes";
+            this.btnVezes.Size = new System.Drawing.Size(68, 61);
+            this.btnVezes.TabIndex = 9;
+            this.btnVezes.Text = "X";
+            this.btnVezes.UseVisualStyleBackColor = true;
+            this.btnVezes.Click += new System.EventHandler(this.btnVezes_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(112, 255);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 17);
+            this.lblResultado.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Resultado: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 395);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnVezes);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnMais);
+            this.Controls.Add(this.txbNumero2);
+            this.Controls.Add(this.txbNumero1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txbNumero1;
+        private System.Windows.Forms.TextBox txbNumero2;
+        private System.Windows.Forms.Button btnMais;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnVezes;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMais_Click(object sender, EventArgs e)
+        {
+            
+            float numero1 = int.Parse( txbNumero1.Text );
+            float numero2 = int.Parse( txbNumero2.Text );
+
+            float resultado;
+
+            resultado = numero1 + numero2;
+
+            lblResultado.Text = resultado.ToString();
+
+        }
+
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+
+            float numero1 = int.Parse(txbNumero1.Text);
+            float numero2 = int.Parse(txbNumero2.Text);
+
+            float resultado;
+
+            resultado = numero1 - numero2;
+
+            lblResultado.Text = resultado.ToString();
+
+        }
+
+        private void btnVezes_Click(object sender, EventArgs e)
+        {
+
+            float numero1 = int.Parse(txbNumero1.Text);
+            float numero2 = int.Parse(txbNumero2.Text);
+
+            float resultado;
+
+            resultado = numero1 * numero2;
+
+            lblResultado.Text = resultado.ToString();
+
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+
+            float numero1 = int.Parse(txbNumero1.Text);
+            float numero2 = int.Parse(txbNumero2.Text);
+
+            float resultado;
+
+            resultado = numero1 / numero2;
+
+            lblResultado.Text = resultado.ToString();
+
+        }
+
+    }
+}
